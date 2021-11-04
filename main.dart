5c2c035817da6ff7df397de0dfbd38afc22c3981
negy <90941752+negy-iby3l@users.noreply.github.com>
2021-11-03 22:55:32 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      // Title
+      title: "TreinaWeb",
+      // Home
+      home: Scaffold(
+        // Color do Fundo
+        backgroundColor: Colors.black,
+        // Appbar
+        appBar: AppBar(
+          // Title
+          title: Text(
+            "TreinaWeb",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        // Body
+        body: Container(
+          // Center the content
+          child: Center(
+            // Add Text
+            child: Text(
+              "Olá, Mundo! :) Isso é flutter!",
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+          ),
+        ),
+      )));
+}
